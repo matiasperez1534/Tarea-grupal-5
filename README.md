219968083b1cc9d950b095d9a3383bb7ceb1abff
matias perez <matipb123@gmail.com>
2023-03-29 22:33:10 -0300

    ejercicioclase11

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trabajo grupal 5 
+
+Integrantes
+
+Rocío Venegas: https://github.com/RoVenegasV/clase11.git
+
+Matías Pérez: https://github.com/matiasperez1534/Tarea-grupal-5.git 
+
+Javier Piñones: https://github.com/JavierPinones/ejercicio5.git
+
+Juan Carlos Gallardo: https://github.com/JCSystemDev/tarea-grupal-5-bootcamp-java
---
+++ ejercicios.js
@@ -0,0 +1,21 @@
+/* Ejercicio 1 - Calcular el IMC*/
+var peso = 75;
+var estatura = 1.8;
+var imc = peso/(estatura*estatura);
+console.log(imc);
+
+/* Ejercicio 2 - Calcular el IVA*/
+var compra = 10000;
+const iva = 0.19
+var compraIVA = compra * iva;
+console.log(compraIVA);
+
+/* Ejercicio 3 - Calcular la hipotenusa dados los catetos*/
+var catop = 3;
+var catad = 4;
+var hipotenusa;
+hipotenusa = Math.sqrt((catop*catop) + (catad*catad)); 
+console.log(hipotenusa);
+
+
+
